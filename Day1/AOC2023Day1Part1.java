d9e604ca8b18ad54df29238625cb3823e6f63331
Jeev0 <139910224+Jeev0@users.noreply.github.com>
2023-12-02 20:50:44 -0500

    Update and rename AdventOfCode2023Day1Part1.java to AOC2023Day1Part1.java

--- Day1/AOC2023Day1Part1.java
+++ Day1/AOC2023Day1Part1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class AdventOfCode2023Day1Part1 
+public class AOC2023Day1Part1 
 {
     public static void main(String args[]) throws FileNotFoundException
     {
